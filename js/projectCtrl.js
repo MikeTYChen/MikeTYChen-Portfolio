c9174586dcc6c90e537cb0dc53d710cea9790b24
Michael Chen <miketychen@gmail.com>
2016-02-17 19:13:52 -0600

    work

--- js/projectCtrl.js
+++ js/projectCtrl.js
@@ -1,10 +1,11 @@
 angular.module('projectCtrl', []).controller('projectController', function($scope,$log) {
 	$scope.activeProject = 0;
 	$scope.projects=[
-	{title:"PackHire",img:"assets/images/PackHire.png",url:"http://www.packhire.com", role:"Software Engineering (Full Stack)"},
-	{title:"HatchGood",img:"assets/images/HatchGood.png",url:"http://www.hatchgood.com", role:"Software Engineering (Full Stack)"},
-	{title:"SEAM",img:"assets/images/SEAM.png",url:"http://seam.herokuapp.com", role:"Software Engineering (Full Stack)"},
-	{title:"Twitter Filter",img:"assets/images/HatchGood.png",url:"http://twitterfilter-miketychen.herokuapp.com/", role:"Software Engineering (Full Stack)"}
+	{title:"PackHire",img:"assets/images/PackHire-Big.png",url:"http://www.packhire.com", role:"Software Engineering (Full Stack)"},
+	{title:"HatchGood",img:"assets/images/HatchGood-Big.png",url:"http://www.hatchgood.com", role:"Software Engineering (Full Stack)"},
+	{title:"SEAM",img:"assets/images/SEAM-Big.png",url:"http://seam.herokuapp.com", role:"Software Engineering (Full Stack)"},
+	{title:"Twitter Filter",img:"assets/images/Twitter-Big.png",url:"http://twitterfilter-miketychen.herokuapp.com/", role:"Software Engineering (Full Stack)"},
+	{title:"Front End",img:"assets/images/Front.png",url:"http://twitterfilter-miketychen.herokuapp.com/", role:"Software Engineering (Full Stack)"},
 	];
 
 
