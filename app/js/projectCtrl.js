85f201b5beb84fe35ae22d3a562538d11050fe10
Michael Chen <miketychen@gmail.com>
2016-02-21 12:12:30 -0600

    carousel images

--- app/js/projectCtrl.js
+++ app/js/projectCtrl.js
@@ -2,11 +2,53 @@ angular.module('projectCtrl', ['ui.bootstrap'])
 .controller('projectController', function($scope,$log) {
 	$scope.activeProject = 0;
 	$scope.projects=[
-	{title:"PackHire",img:"assets/images/PackHire-Big.png",url:"http://www.packhire.com", role:"Software Engineering (Full Stack)"},
-	{title:"HatchGood",img:"assets/images/HatchGood-Big.png",url:"http://www.hatchgood.com", role:"Software Engineering (Full Stack)"},
-	{title:"SEAM",img:"assets/images/SEAM-Big.png",url:"http://seam.herokuapp.com", role:"Software Engineering (Full Stack)"},
-	{title:"Twitter Filter",img:"assets/images/Twitter-Big.png",url:"http://twitterfilter-miketychen.herokuapp.com/", role:"Software Engineering (Full Stack)"},
-	{title:"Front End",img:"assets/images/Front.png",url:"http://twitterfilter-miketychen.herokuapp.com/", role:"Software Engineering (Full Stack)"},
+	{title:"PackHire",img:"assets/images/PackHire-Big.png",url:"http://www.packhire.com", role:"Software Engineering (Full Stack)",
+    slides:[
+      {image:"assets/images/PackHire/pack-0.png"},
+      {image:"assets/images/PackHire/pack-1.jpeg"},
+      {image:"assets/images/PackHire/pack-2.jpeg"},
+      {image:"assets/images/PackHire/pack-3.jpeg"},
+      {image:"assets/images/PackHire/pack-4.jpeg"},
+      {image:"assets/images/PackHire/pack-5.jpeg"},
+      {image:"assets/images/PackHire/pack-6.jpeg"},
+    ]
+  },
+	{title:"HatchGood",img:"assets/images/HatchGood-Big.png",url:"http://www.hatchgood.com", role:"Software Engineering (Full Stack)",
+    slides:[
+      {image:"assets/images/HatchGood/hatch-0.png"},
+      {image:"assets/images/HatchGood/hatch-1.jpeg"},
+      {image:"assets/images/HatchGood/hatch-2.jpeg"},
+      {image:"assets/images/HatchGood/hatch-3.jpeg"},
+      {image:"assets/images/HatchGood/hatch-4.jpeg"},
+      {image:"assets/images/HatchGood/hatch-5.jpeg"},
+    ]
+  },
+	{title:"SEAM",img:"assets/images/SEAM-Big.png",url:"http://seam.herokuapp.com", role:"Software Engineering (Full Stack)",
+    slides:[
+      {image:"assets/images/SEAM/seam-0.png"},
+      {image:"assets/images/SEAM/seam-1.jpeg"},
+      {image:"assets/images/SEAM/seam-2.jpeg"},
+      {image:"assets/images/SEAM/seam-3.jpeg"},
+      {image:"assets/images/SEAM/seam-4.jpeg"},
+      {image:"assets/images/SEAM/seam-5.jpeg"},
+    ]  
+  },
+	{title:"Twitter Filter",img:"assets/images/Twitter-Big.png",url:"http://twitterfilter-miketychen.herokuapp.com/", role:"Software Engineering (Full Stack)",
+    slides:[
+      {image:"assets/images/Twitter/twitter-0.png"},
+      {image:"assets/images/Twitter/twitter-1.jpeg"},
+      {image:"assets/images/Twitter/twitter-2.jpeg"},
+    ]
+  },
+	{title:"Front End",img:"assets/images/Front.png",url:"http://twitterfilter-miketychen.herokuapp.com/", role:"Software Engineering (Full Stack)",
+    slides:[
+      {image:"assets/images/Front/front-0.png"},
+      {image:"assets/images/Front/front-1.jpeg"},
+      {image:"assets/images/Front/front-2.jpeg"},
+      {image:"assets/images/Front/front-3.jpeg"},
+      {image:"assets/images/Front/front-4.jpeg"}
+    ]
+  },
 	];
 
 
@@ -17,12 +59,16 @@ angular.module('projectCtrl', ['ui.bootstrap'])
 	$scope.isActive = function(project){
         return $scope.projects.indexOf(project) == $scope.activeProject;
     }
-})
-.controller('CarouselCtrl', function ($scope) {
   $scope.myInterval = 5000;
   $scope.noWrapSlides = false;
-  var slides = $scope.slides = [{image:"assets/images/PackHire-Big.png"},{image:"assets/images/HatchGood-Big.png"}];
+  var slides = $scope.slides = $scope.projects[$scope.activeProject].slides;
   var currIndex = 0;
+  $scope.updateSlide = function(){
+    $scope.slides = $scope.projects[$scope.activeProject].slides;
+  }
+})
+.controller('CarouselCtrl', function ($scope,$log) {
+  
 })
 .config(['$provide', Decorate]);
   function Decorate($provide) {
