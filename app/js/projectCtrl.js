4048c04891fe5545940bfa6b3258015cbfa4c1aa
Michael Chen <miketychen@gmail.com>
2016-02-21 21:19:20 -0600

    project desc

--- app/js/projectCtrl.js
+++ app/js/projectCtrl.js
@@ -3,6 +3,8 @@ angular.module('projectCtrl', ['ui.bootstrap'])
 	$scope.activeProject = 0;
 	$scope.projects=[
 	{title:"PackHire",img:"assets/images/PackHire-Big.png",url:"http://www.packhire.com", role:"Software Engineering (Full Stack)",
+    tech:["HTML","CSS","Javascript","Gulp","Ruby on Rails","PostgreSQL"],
+    about:"PackHire provides you with platform where you can turn people in your network into the best recruiters for your company",
     slides:[
       {image:"assets/images/PackHire/pack-0.png"},
       {image:"assets/images/PackHire/pack-1.jpeg"},
@@ -14,6 +16,8 @@ angular.module('projectCtrl', ['ui.bootstrap'])
     ]
   },
 	{title:"HatchGood",img:"assets/images/HatchGood-Big.png",url:"http://www.hatchgood.com", role:"Software Engineering (Full Stack)",
+    tech:["HTML","CSS/SASS","Javascript","Gulp","Node.JS","MySQL"],
+    about:"PackHire provides you with platform where you can turn people in your network into the best recruiters for your company",
     slides:[
       {image:"assets/images/HatchGood/hatch-0.png"},
       {image:"assets/images/HatchGood/hatch-1.jpeg"},
@@ -24,6 +28,8 @@ angular.module('projectCtrl', ['ui.bootstrap'])
     ]
   },
 	{title:"SEAM",img:"assets/images/SEAM-Big.png",url:"http://seam.herokuapp.com", role:"Software Engineering (Full Stack)",
+    tech:["HTML","CSS","Javascript","Gulp","Node.JS","MongoDB"],
+    about:"PackHire provides you with platform where you can turn people in your network into the best recruiters for your company",
     slides:[
       {image:"assets/images/SEAM/seam-0.png"},
       {image:"assets/images/SEAM/seam-1.jpeg"},
@@ -34,6 +40,8 @@ angular.module('projectCtrl', ['ui.bootstrap'])
     ]  
   },
 	{title:"Twitter Filter",img:"assets/images/Twitter-Big.png",url:"http://twitterfilter-miketychen.herokuapp.com/", role:"Software Engineering (Full Stack)",
+    tech:["HTML","CSS","Javascript","Python"],
+    about:"PackHire provides you with platform where you can turn people in your network into the best recruiters for your company",
     slides:[
       {image:"assets/images/Twitter/twitter-0.png"},
       {image:"assets/images/Twitter/twitter-1.jpeg"},
@@ -41,6 +49,8 @@ angular.module('projectCtrl', ['ui.bootstrap'])
     ]
   },
 	{title:"Front End",img:"assets/images/Front.png",url:"http://twitterfilter-miketychen.herokuapp.com/", role:"Software Engineering (Full Stack)",
+    tech:["HTML","CSS","SASS","Javascript","Angular.JS","Gulp"],
+    about:"PackHire provides you with platform where you can turn people in your network into the best recruiters for your company",
     slides:[
       {image:"assets/images/Front/front-0.png"},
       {image:"assets/images/Front/front-1.jpeg"},
@@ -50,6 +60,8 @@ angular.module('projectCtrl', ['ui.bootstrap'])
     ]
   },
   {title:"Data Science",img:"assets/images/Front.png",url:"#", role:"Software Engineering & Data Science",
+    tech:["Java","Python","C++","MySQL"],
+    about:"PackHire provides you with platform where you can turn people in your network into the best recruiters for your company",
     slides:[
       {image:"assets/images/Front/front-0.png"},
       {image:"assets/images/Front/front-1.jpeg"},
