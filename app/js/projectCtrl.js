5aabcdda1a11602f06f10ccac75139bf0fc8bac3
Michael Chen <miketychen@gmail.com>
2016-03-24 08:29:29 -0500

    add content

--- app/js/projectCtrl.js
+++ app/js/projectCtrl.js
@@ -5,6 +5,7 @@ angular.module('projectCtrl', ['ui.bootstrap'])
 	{title:"PackHire",img:"assets/images/PackHire-Big.png",url:"http://www.packhire.com", role:"Software Engineering (Full Stack)",
     tech:["HTML","CSS","Javascript","Gulp","Ruby on Rails","PostgreSQL"],
     about:"PackHire provides you with platform where you can turn people in your network into the best recruiters for your company",
+    desc:"Some of the features include social media integration, referral tracking, algorithmic scoring of candidates and referrals, and intuitive dashboard for campaign managers to manage job listings.",
     slides:[
       {image:"assets/images/PackHire/pack-0.jpg"},
       {image:"assets/images/PackHire/pack-1.jpg"},
@@ -17,7 +18,8 @@ angular.module('projectCtrl', ['ui.bootstrap'])
   },
 	{title:"SEAM",img:"assets/images/SEAM-Big.png",url:"http://seam.herokuapp.com", role:"Software Engineering (Full Stack)",
     tech:["HTML","CSS","Javascript","Gulp","Node.JS","MongoDB"],
-    about:"PSEAM is a platform that eases the pain of meetings. It helps a user plan for focused discussions, gives the power to move conversations forward and align the team on next steps afterwards.",
+    about:"SEAM is a platform that eases the pain of meetings. It helps a user plan for focused discussions, gives the power to move conversations forward and align the team on next steps afterwards.",
+    desc:"Some of the features include real-time communication (Socket.IO), auto generation of meeting notes, and auto-scheduling of meeting times.",
     slides:[
       {image:"assets/images/SEAM/seam-0.jpg"},
       {image:"assets/images/SEAM/seam-1.jpg"},
@@ -30,6 +32,7 @@ angular.module('projectCtrl', ['ui.bootstrap'])
 	{title:"Twitter Filter",img:"assets/images/Twitter-Big.png",url:"http://twitterfilter-miketychen.herokuapp.com/", role:"Software Engineering (Full Stack)",
     tech:["HTML","CSS","Javascript","Python"],
     about:"The Twitter Filter project was a short project that enables a user to pull any user's tweet and filter based on words or date it was tweeted.",
+    desc:"Some of the features include filtering of tweets and requests tweets via Twitter API.",
     slides:[
       {image:"assets/images/Twitter/twitter-0.jpg"},
       {image:"assets/images/Twitter/twitter-1.jpg"},
@@ -39,6 +42,7 @@ angular.module('projectCtrl', ['ui.bootstrap'])
 	{title:"Front End",img:"assets/images/Front.png",url:"http://twitterfilter-miketychen.herokuapp.com/", role:"Front-End Engineering",
     tech:["HTML","CSS","SASS","Javascript","Angular.JS","Gulp"],
     about:"My front end projects consists of creating multiple sites for clients as well as organizations to display data and content in a meaningful and visually optimized way.",
+    desc:"Some of the features include task automation, fully responsive, and form generation to Google Sheets for analysis.",
     slides:[
       {image:"assets/images/Front/front-0.jpg"},
       {image:"assets/images/Front/front-1.jpg"},
@@ -50,6 +54,7 @@ angular.module('projectCtrl', ['ui.bootstrap'])
   {title:"Data Science",img:"assets/images/Front.png",url:"#", role:"Software Engineering & Data Science",
     tech:["Java","Python","C++","MySQL"],
     about:"My data science and bioinformatics projects include: developing algorithms to optimize engagement and insights on social ads, create pipelines to sequence the genome and map out 3D architecture of whole genomes interactions.",
+    desc:"Some of the features include analyzing 5GB of data and statistically filtering and correcting data, visual analysis of all 23 human chromosomes interactions internally and externally, and utilization of Facebook Graph API.",
     slides:[
       {image:"assets/images/DataScience/data-0.jpg"},
       {image:"assets/images/DataScience/data-1.jpg"},
@@ -61,6 +66,7 @@ angular.module('projectCtrl', ['ui.bootstrap'])
   {title:"HatchGood",img:"assets/images/HatchGood-Big.png",url:"http://www.hatchgood.com", role:"Software Engineering (Full Stack)",
     tech:["HTML","CSS/SASS","Javascript","Gulp","Node.JS","MySQL"],
     about:"HatchGood is an e-commerce platform that aims to bring great design and new talent to consumer needs. By providing a platform for designers to sell their ideas, consumers are able to purchase well-designed products with a fraction of the cost.",
+    desc:"Some of the features include community driven products, and multiple listings.",
     slides:[
       {image:"assets/images/HatchGood/hatch-0.jpg"},
       {image:"assets/images/HatchGood/hatch-1.jpg"},
