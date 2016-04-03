6869db54581fbfe3c972eecceb805e405188d5e1
Michael Chen <miketychen@gmail.com>
2016-04-02 21:58:55 -0500

    v3

---
+++ app/views/partials/general/_particle.js
@@ -0,0 +1,3 @@
+<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+<script src="js/particle.js"></script>
+<script src="js/data.js"></script>
--- public/js/main.js
+++ public/js/main.js
@@ -5,11 +5,27 @@ $( document ).ready(function() {
 
 function fixed_nav (){
 	var pos = $(window).scrollTop();
-	var panel1 = $('.top-project-panel').offset().top;
-	var boolPos;
+	var panel1 = $('.top-project-panel').offset().top-50;
 	if(pos>panel1){
-		$( "#toggle-nav" ).show( "drop", {direction: "up"}, 250 );
+		$( "#toggle-nav" ).css({'background-color': '#232323'});
+		// $( "#toggle-nav" ).show( "drop", {direction: "up"}, 250 );
 	}else{
-		$( "#toggle-nav" ).hide( "slide", {direction: "up"}, 250 );
+		$( "#toggle-nav" ).css({'background-color': 'transparent'});
+		// $( "#toggle-nav" ).hide( "slide", {direction: "up"}, 250 );
 	}
 }
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
