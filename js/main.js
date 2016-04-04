dfd14da932928e256fed3ece68b7003fab72fc0f
Michael Chen <miketychen@gmail.com>
2016-04-04 15:34:11 -0500

    final revisions

--- js/main.js
+++ js/main.js
@@ -1,35 +1,34 @@
 $( document ).ready(function() {
-    console.log("HI");
     $(document).on('click','.navbar-collapse',function(e) {
         console.log("close");
         if( $(e.target).is('a') ) {
             $(this).collapse('hide');
         }
     });
-        $( window ).scroll(fixed_nav);
-        fixed_nav();
-        $(document).on("scroll", onScroll);
+    $( window ).scroll(fixed_nav);
+    fixed_nav();
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
         
-        //smoothscroll
-        $('a[href^="#"]').on('click', function (e) {
-            e.preventDefault();
-            $(document).off("scroll");
-            
-            $('a').each(function () {
-                $(this).removeClass('active');
-            })
-            $(this).addClass('active');
-          
-            var target = this.hash,
-                menu = target;
-            $target = $(target);
-            $('html, body').stop().animate({
-                'scrollTop': $target.offset().top+2
-            }, 500, 'swing', function () {
-                window.location.hash = target;
-                $(document).on("scroll", onScroll);
-            });
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
         });
+    });
 });
 
 function fixed_nav (){
